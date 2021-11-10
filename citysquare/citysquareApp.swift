c86fd68c9aad8836f62f71d58603a25bfd0fa23f
Maria Garcia Luque <mariaparadigma@paradigmadigital.com>
2021-11-10 18:49:05 +0100

    Initial Commit

---
+++ citysquare/citysquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  citysquareApp.swift
+//  citysquare
+//
+//  Created by Maria Garcia Luque on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct citysquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
